8b78309fffef483b871f9d9614c442f2767980f2
WyattMarks <wmarks@siue.edu>
2021-10-16 10:18:22 -0500

    GPS + Nav Drawer

--- test/main.py
+++ test/main.py
@@ -1,100 +1,134 @@
-
 from kivy.lang import Builder
+from kivy.properties import StringProperty, ListProperty
 from plyer import gps
-from kivy.app import App
-from kivy.properties import StringProperty
+from kivymd.app import MDApp
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.list import OneLineIconListItem, MDList
 from kivy.clock import mainthread
 from kivy.utils import platform
 
-kv = '''
-BoxLayout:
-    orientation: 'vertical'
-    Label:
-        text: app.gps_location
-    Label:
-        text: app.gps_status
-    BoxLayout:
+KV = '''
+# Menu item in the DrawerList list.
+<ItemDrawer>:
+    theme_text_color: "Custom"
+    on_release: self.parent.set_color_item(self)
+    IconLeftWidget:
+        id: icon
+        icon: root.icon
+        theme_text_color: "Custom"
+        text_color: root.text_color
+<ContentNavigationDrawer>:
+    orientation: "vertical"
+    padding: "8dp"
+    spacing: "8dp"
+    AnchorLayout:
+        anchor_x: "left"
         size_hint_y: None
-        height: '48dp'
-        padding: '4dp'
-        ToggleButton:
-            text: 'Start' if self.state == 'normal' else 'Stop'
-            on_state:
-                app.start(1000, 0) if self.state == 'down' else \
-                app.stop()
+        height: avatar.height
+        Image:
+            id: avatar
+            size_hint: None, None
+            size: "56dp", "56dp"
+            source: "data/logo/kivy-icon-256.png"
+    MDLabel:
+        text: "KivyMD library"
+        font_style: "Button"
+        adaptive_height: True
+    MDLabel:
+        text: "kivydevelopment@gmail.com"
+        font_style: "Caption"
+        adaptive_height: True
+    ScrollView:
+        DrawerList:
+            id: md_list
+MDScreen:
+    MDNavigationLayout:
+        ScreenManager:
+            MDScreen:
+                MDBoxLayout:
+                    orientation: 'vertical'
+                    MDToolbar:
+                        title: "Navigation Drawer"
+                        elevation: 10
+                        left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
+                    Widget:
+        MDNavigationDrawer:
+            id: nav_drawer
+            ContentNavigationDrawer:
+                id: content_drawer
 '''
 
 
-class GpsTest(App):
+class ContentNavigationDrawer(MDBoxLayout):
+    pass
 
-    gps_location = StringProperty()
-    gps_status = StringProperty('Click Start to get GPS location updates')
+
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+    text_color = ListProperty((0, 0, 0, 1))
+
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """Called when tap on a menu item."""
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+
+
+class TestNavigationDrawer(MDApp):
 
     def request_android_permissions(self):
-        """
-        Since API 23, Android requires permission to be requested at runtime.
-        This function requests permission and handles the response via a
-        callback.
-        The request will produce a popup if permissions have not already been
-        been granted, otherwise it will do nothing.
-        """
         from android.permissions import request_permissions, Permission
-
-        def callback(permissions, results):
-            """
-            Defines the callback to be fired when runtime permission
-            has been granted or denied. This is not strictly required,
-            but added for the sake of completeness.
-            """
-            if all([res for res in results]):
-                print("callback. All permissions granted.")
-            else:
-                print("callback. Some permissions refused.")
-
-        request_permissions([Permission.ACCESS_COARSE_LOCATION,
-                             Permission.ACCESS_FINE_LOCATION], callback)
-        # # To request permissions without a callback, do:
-        # request_permissions([Permission.ACCESS_COARSE_LOCATION,
-        #                      Permission.ACCESS_FINE_LOCATION])
+        
+        request_permissions([Permission.ACCESS_COARSE_LOCATION, Permission.ACCESS_FINE_LOCATION])
 
     def build(self):
         try:
-            gps.configure(on_location=self.on_location,
-                          on_status=self.on_status)
+            gps.configure(on_location=self.location_update,
+            on_status=self.status_update)
         except NotImplementedError:
             import traceback
             traceback.print_exc()
-            self.gps_status = 'GPS is not implemented for your platform'
+            self.status_update("GPS Not Implemented")
 
         if platform == "android":
-            print("gps.py: Android detected. Requesting permissions")
+            print("Android! Requesting Permissions")
             self.request_android_permissions()
 
-        return Builder.load_string(kv)
+        return Builder.load_string(KV)
 
-    def start(self, minTime, minDistance):
-        gps.start(minTime, minDistance)
-
-    def stop(self):
-        gps.stop()
+    def on_start(self):
+        icons_item = {
+            "folder": "My files",
+            "account-multiple": "Shared with me",
+            "star": "Starred",
+            "history": "Recent",
+            "checkbox-marked": "Shared with me",
+            "upload": "Upload",
+        }
+        for icon_name in icons_item.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
+        gps.start(1000, 0)
 
     @mainthread
-    def on_location(self, **kwargs):
-        self.gps_location = '\n'.join([
-            '{}={}'.format(k, v) for k, v in kwargs.items()])
+    def location_update(self, **kwargs):
+        print("YO DEBUG MESSAGE")
+        print('\n'.join([
+            '{}={}'.format(k, v) for k, v in kwargs.items()]))
+        self.lat = kwargs['lat']
+        sefl.lon = kwargs['lon']
 
     @mainthread
-    def on_status(self, stype, status):
+    def status_update(self, stype, status):
         self.gps_status = 'type={}\n{}'.format(stype, status)
 
-    def on_pause(self):
-        gps.stop()
-        return True
 
-    def on_resume(self):
-        gps.start(1000, 0)
-        pass
-
-
-if __name__ == '__main__':
-    GpsTest().run()
+TestNavigationDrawer().run()
